f15b687c4956b6e51c8bd429aed72bb80239ce75
Javier_Acosta <jacosta@catamarca.gov.ar>
2025-05-05 11:43:55 -0300

    render

--- src/api/clienteApi.jsx
+++ src/api/clienteApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api= axios.create ({
     // baseURL: 'http://localhost:3000/api',
-    baseURL: 'https://server-cbbf.onrender.com/',
+    baseURL: 'server-cbbf.onrender.com/',
 });
 
 export const loginUser = (credentials) => api.post (`/auth/login`, credentials);
